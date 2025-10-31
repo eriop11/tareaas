95abefb14c19fd207c5517b40743757df43f0f3b
eriop11 <133905712+eriop11@users.noreply.github.com>
2025-10-31 20:59:02 -0300

    Update and rename main.tsx to app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dashboard Principal",
+    page_icon="🚀",
+    layout="wide"
+)
+
+def main():
+
+if __name__ == "__main__":
+    main()
